dafcb195de2f5cbd771d32e2fc552cdd920ffb53
Cândido Sales Gomes <candidosg@gmail.com>
2016-05-19 15:12:41 -0300

    basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: "quake@brasilct.com", password: "brasilct-2016" 
 end
