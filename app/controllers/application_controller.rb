cbce0b2f70016f7f626a55038fa26d08923029db
Cândido Sales Gomes <candidosg@gmail.com>
2016-05-20 13:44:57 -0300

    basic authentication in just home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: "quake@brasilct.com", password: "brasilct-2016" 
 end
--- app/controllers/duckrails/home_controller.rb
+++ app/controllers/duckrails/home_controller.rb
@@ -1,5 +1,7 @@
 module Duckrails
   class HomeController < ApplicationController
+    http_basic_authenticate_with name: "quake@brasilct.com", password: "brasilct-2016" 
+
     def index
     end
   end
